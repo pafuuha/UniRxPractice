57210380658c0c0f6a03e24685fc2c651bc05587
パフーハ <pafuuha45542@gmail.com>
2019-10-03 22:29:21 +0900

    ひとまず、ダブルクリックの例をいじる

---
+++ UniRxPractice/Assets/Script/UniRxMyTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.UI;
+
+public class UniRxMyTest : MonoBehaviour
+{
+    [SerializeField]
+    private Button TestButton;
+
+    [SerializeField]
+    private Text TestText;
+
+    int count = 0;
+    bool stateCheck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var stream = this.UpdateAsObservable()
+            .Where(a => Input.GetMouseButton(0));
+
+        var otherStream = stream.Throttle(TimeSpan.FromMilliseconds(200f));
+
+        stream.Buffer(otherStream)
+            .Where(x => x.Count >= 2)
+            .Subscribe(a =>
+            {
+                Debug.Log("Test");
+                Debug.Log(a.Count);
+            });
+
+        this.UpdateAsObservable()
+            .Select(a=>stateCheck)
+            .DistinctUntilChanged()
+            .Where(o=>o == true)
+            .Subscribe(o=>{
+                Debug.Log(o);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            stateCheck = !stateCheck;
+        }
+    }
+}
