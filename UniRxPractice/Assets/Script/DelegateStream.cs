a3d26801cb0793100d88fa40f91b202b390171cb
パフーハ <pafuuha45542@gmail.com>
2019-10-04 00:39:19 +0900

    delegateをストリームに変換するのを調査

---
+++ UniRxPractice/Assets/Script/DelegateStream.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class DelegateStream : MonoBehaviour
+{
+
+    public delegate void SomeEvent(string msg);
+    public SomeEvent someEvent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observable.FromEvent<SomeEvent, string>(
+            h => msg => h(msg),
+            h => someEvent += h,
+            h => someEvent -= h
+        )
+        .Subscribe(x => Debug.Log("OnComplateCallback:" + x));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
